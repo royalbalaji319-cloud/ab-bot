939036955392e5a0257312a84729c0bfb8af22a7
royalbalaji319-cloud <royalbalaji319@gmail.com>
2026-02-07 12:17:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ def chat():
         return jsonify({"reply": f"❌ Error: {str(e)}"})
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+
 
